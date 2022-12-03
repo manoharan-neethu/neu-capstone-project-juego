07820df5370b6bb2c0e0e9a2ae94734854a6d402
manoharan.n@northeastern.edu <redforkey@gmail.com>
2022-12-03 11:56:27 -0500

    Restructure fodlers

--- 0-Initial-Setup-Stage/config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Do not use quotes
-GCP_SERVICE_ACCOUNT=ctf-infra-manager@juego-ctf-2022-cp.iam.gserviceaccount.com
-GCP_SERVICE_ACCOUNT_KEY=~/.ssh/juego-ctf-2022-cp-e29ec6775f69.json
-GCP_PROJECT_ID=juego-ctf-2022-cp
